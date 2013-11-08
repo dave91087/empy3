46bd95593e8e973d7e7e919655ef5a7df2f13ece
dave91087 <dave91087@gmail.com>
2013-11-08 09:50:08 +0000

    Cleanup and basic error handling

--- empy3.py
+++ empy3.py
@@ -8,53 +8,41 @@ except ImportError:
     # Mutagen 1.17.
     from mutagenx.id3 import EasyID3FileType
 
-indir = 'C:\\TEMP\\Music'
-tempdir = 'C:\\TEMP\\NewMusic\\'
+# Directories
+fromDir = 'C:\\TEMP\\Music\\'
+toDir = 'C:\\TEMP\\NewMusic\\'
+
+# Logfile of sorts
 f = open("C:\\TEMP\\mp3s.txt", 'a')
 
-for root, dirs, filenames in os.walk(indir):
+for root, dirs, filenames in os.walk(fromDir):
     for fn in filenames:
         if os.path.isfile(os.path.join(root,fn)) and fn.endswith('.mp3'):
-            test = mutagenx.File(os.path.join(root,fn), easy=True)
-            print(fn+"\n")
-            writestr = fn+"\t"+str(test["artist"])+"\t"+str(test["album"])+"\t"+str(test["tracknumber"])+"\t"+str(test["title"])+"\n"
-            writestr = writestr.replace('[','').replace(']','')
-            newpath =  tempdir+str(test["artist"])+"\\"+str(test["album"])+"\\"
-            newname = str(test["title"])+".mp3"
-            newpath = newpath.replace('[','').replace(']','').replace('\'','').replace('"','')
-            newname = newname.replace('[','').replace(']','').replace('\'','').replace('"','').replace('/', '_').replace('?', '')
-            f.write(writestr)
-            f.write('"'+newpath+newname+'"\n')
-            f.flush()
-            writestr = ''
-            if os.path.exists(newpath):
-                shutil.copyfile(os.path.join(root,fn), newpath+newname)
-            else:
-                os.makedirs(newpath)
-                shutil.copyfile(os.path.join(root,fn), newpath+newname)
+            try:
+                test = mutagenx.File(os.path.join(root,fn), easy=True)
 
+                #Output to console to show progress
+                print(fn+"\n")
 
+                newpath =  toDir+str(test["artist"])+"\\"+str(test["album"])+"\\"
+                newpath = newpath.replace('[','').replace(']','').replace('\'','').replace('"','')
+
+                newname = str(test["title"])+".mp3"
+                newname = newname.replace('[','').replace(']','').replace('\'','').replace('"','').replace('/', '_').replace('?', '')
+                f.write('"'+newpath+newname+'"\n')
+                f.flush()
+
+                if os.path.exists(newpath):
+                    shutil.copyfile(os.path.join(root,fn), newpath+newname)
+                else:
+                    os.makedirs(newpath)
+                    shutil.copyfile(os.path.join(root,fn), newpath+newname)
+            except:
+                f.write('Error: '+fn+"\n")
+                print('Error: '+fn)
+                f.flush()
         f.close
-            #print(test.pprint())
 
-
-
-
-
-#            print("Filepath "+os.path.join(root,fn))
-#            print("Is MP3? "+str(fn.endswith('.mp3')))
-#            print(fn);
-            ##audio = MP3(os.path.join(root,fn), ID3=EasyID3)
-            ##songtitle = audio["title"]
-            ##artist = audio["performer"]
-            ##album = audio["album"]
-            ##genre = audio["genre"]
-            ##encodedby = audio["author"]
-
-            ##print(encodedby)
-            #print(artist)
-            #print(album)
-            #print(audio.info.length, audio.info.bitrate)
-            #audiofile = eyed3.load(os.path.join(root,fn))
-            #print(file_obj)
-
+# Unused Logging...
+#writestr = fn+"\t"+str(test["artist"])+"\t"+str(test["album"])+"\t"+str(test["tracknumber"])+"\t"+str(test["title"])+"\n"
+#writestr = writestr.replace('[','').replace(']','')
